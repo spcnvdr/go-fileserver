5108d95fca512a95b76f1e78d305c68ca26a8504
Stephan Hegemann <stephanhegemann@pm.me>
2025-03-22 22:14:45 +0100

    fixed a bug where firefox displays an error saying "The page isn’t redirecting properly..Firefox has detected that the server is redirecting the request for this address in a way that will never complete."

--- cmd/main.go
+++ cmd/main.go
@@ -434,7 +434,7 @@ func checkForPathTraversal(path string, client_addr string) bool {
 
 // redirectRoot redirects server root to /view?dir=/.
 func redirectRoot(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "/view?dir=/", http.StatusFound)
+	http.Redirect(w, r, "/view?dir=./", http.StatusFound)
 }
 
 // getFile serves a single file requested via URL
@@ -600,7 +600,7 @@ func viewDir(w http.ResponseWriter, r *http.Request) {
 	// What is the parent for current folder?
 	parent := filepath.Dir(dir)
 	if parent == "." {
-		parent = "/"
+		parent = "./"
 	}
 
 	if checkForPathTraversal(dir, r.RemoteAddr) {
